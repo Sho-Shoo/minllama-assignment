0706dcff59f3007b1cab414ba58deee50479c7ef
Tianze Shou <shoutianze@outlook.com>
2024-02-09 12:59:11 -0500

    Slightly off implementation

--- optimizer.py
+++ optimizer.py
@@ -38,7 +38,7 @@ class AdamW(Optimizer):
                 if grad.is_sparse:
                     raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
 
-                raise NotImplementedError()
+                # raise NotImplementedError()
 
                 # State should be stored in this dictionary
                 state = self.state[p]
@@ -47,12 +47,31 @@ class AdamW(Optimizer):
                 alpha = group["lr"]
 
                 # Update first and second moments of the gradients
+                if "first_moment" not in state:
+                    state["first_moment"] = torch.zeros(grad.shape)
+                if "second_moment" not in state:
+                    state["second_moment"] = torch.zeros(grad.shape)
+                if "timestep" not in state:
+                    state["timestep"] = 0
+
+                beta1, beta2 = self.defaults["betas"][0], self.defaults["betas"][1]
+                state["timestep"] += 1
+                state["first_moment"] = beta1 * state["first_moment"] + (1 - beta1) * grad
+                state["second_moment"] = beta2 * state["second_moment"] + (1 - beta2) * grad * grad
 
                 # Bias correction
                 # Please note that we are using the "efficient version" given in
                 # https://arxiv.org/abs/1412.6980
+                if self.defaults["correct_bias"]:
+                    m_hat = state["first_moment"] / (1 - beta1 ** state["timestep"])
+                    v_hat = state["second_moment"] / (1 - beta2 ** state["timestep"])
+                else:
+                    m_hat, v_hat = state["first_moment"], state["second_moment"]
 
                 # Update parameters
+                eps = self.defaults["eps"]
+                lam = self.defaults["weight_decay"]
+                p.data -= alpha * m_hat / (torch.sqrt(v_hat) + eps) + lam * p.data
 
                 # Add weight decay after the main gradient-based updates.
                 # Please note that the learning rate should be incorporated into this update.
